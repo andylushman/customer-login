6e1ce039ded4cc04070c776ec879b77aed8946f5
Andy Lushman <andylushman@gmail.com>
2018-01-09 21:53:24 -0700

    Playing with ways of styling

---
+++ customer-login/src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+
+  }
+
+  render() {
+    return (
+
+      <div className="App">
+        <h1>Title</h1>
+        <p>Smaller title</p>
+      </div>
+    );
+  }
+}
+
+export default App;
