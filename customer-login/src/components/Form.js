0c641136db61cdba8b5a276567084ca198b29056
Andy Lushman <andylushman@gmail.com>
2018-01-10 20:25:38 -0700

    Created baic form userpage components

---
+++ customer-login/src/components/Form.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+  constructor() {
+    super();
+
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default Form;
---
+++ customer-login/src/components/UserPage.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class UserPage extends Component {
+  constructor() {
+    super();
+
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    );
+  }
+}
+
+export default UserPage;
